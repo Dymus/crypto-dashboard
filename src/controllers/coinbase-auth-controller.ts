d282da3ab85f9972994f916dbd2001c43820b7c1
Sebastian Holesz <1081515@ucn.dk>
2021-03-12 16:24:16 +0100

    added redirect to frontend

--- src/controllers/coinbase-auth-controller.ts
+++ src/controllers/coinbase-auth-controller.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 import { UserModel } from "../models/user-model";
 
 export const getCoinbaseLogin: RequestHandler = async (req, res, next) => {
-  res.redirect(`http://www.coinbase.com/oauth/authorize?response_type=code&client_id=920061065859143b276c9118f33e3cd48bde5ededaa8e2cfc0ef8ac5d7fb51db&redirect_uri=http://localhost:3000/coinbase/redirect&scope=wallet:accounts:read,wallet:sells:read,wallet:buys:read`);
+  console.log("it hit me  ")
+  res.redirect(`http://www.coinbase.com/oauth/authorize?response_type=code&client_id=920061065859143b276c9118f33e3cd48bde5ededaa8e2cfc0ef8ac5d7fb51db&redirect_uri=http://localhost:3000/coinbase/redirect&scope=wallet:accounts:read,wallet:sells:read,wallet:buys:read,wallet:deposits:read&account=all`);
 };
 
 export const getCoinbaseRedirect: RequestHandler = async (req, res, next) => {
@@ -15,7 +16,8 @@ export const getCoinbaseRedirect: RequestHandler = async (req, res, next) => {
         user.token = response.data
         user.save()
           .then(() => {
-            return res.status(200).send();
+            return res.redirect(
+              `http://localhost:8080/coinbase-login-success/${response.data.access_token}`)
           })
       },
       (error) => { return res.status(error.response.status) }
