67d00f17b33dc870201f84e1ce3ac9c05f0696e9
dymus <damian.hrabaszcz@op.pl>
2021-04-17 19:31:41 +0200

    Implemented backend for setting alerts for notifications

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,5 +1,25 @@
 import { RequestHandler } from 'express';
+import { User, UserModel } from '../models/user-model';
+import { DocumentType } from "@typegoose/typegoose";
 
 export const getUserAuthStatus: RequestHandler = (req, res, _) => {
   return res.status(200).json({isCoinbaseApproved: req.user.coinbaseTokens ? true : false, isGeminiApproved: req.user.geminiKeys ? true : false})
 };
+
+export const getUserAlerts: RequestHandler = (req, res, _) => {
+  return res.status(200).json({alerts: req.user.alerts});
+}
+
+export const setUserAlerts: RequestHandler = (req, res, _) => {
+  const filter = {_id: req.user._id};
+  const update = {alerts: req.body};
+
+  UserModel.findOneAndUpdate(filter, update, {new: true})
+  .then(user => {
+    if (JSON.stringify(user.alerts) === JSON.stringify(req.body)) {
+      return res.status(200).json({wasSuccess: true})
+    } else {
+      return res.status(400).json({error: 'Unknown error while updating user alerts'})
+    }
+  })
+}
