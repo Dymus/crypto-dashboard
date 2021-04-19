ac31acb6df94e9e96ab5b7fe6b47e4f2bb006af3
dymus <damian.hrabaszcz@op.pl>
2021-04-19 15:02:20 +0200

    Added RequestError with message when saving alerts fails

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import { User, UserModel } from '../models/user-model';
 import { DocumentType } from "@typegoose/typegoose";
+import { RequestError } from '../types/RequestError';
 
 export const getUserAuthStatus: RequestHandler = (req, res, _) => {
   return res.status(200).json({isCoinbaseApproved: req.user.coinbaseTokens ? true : false, isGeminiApproved: req.user.geminiKeys ? true : false})
@@ -10,7 +11,7 @@ export const getUserAlerts: RequestHandler = (req, res, _) => {
   return res.status(200).json({alerts: req.user.alerts});
 }
 
-export const setUserAlerts: RequestHandler = (req, res, _) => {
+export const setUserAlerts: RequestHandler = (req, res, next) => {
   const filter = {_id: req.user._id};
   const update = {alerts: req.body};
 
@@ -19,7 +20,7 @@ export const setUserAlerts: RequestHandler = (req, res, _) => {
     if (JSON.stringify(user.alerts) === JSON.stringify(req.body)) {
       return res.status(200).json({wasSuccess: true})
     } else {
-      return res.status(400).json({error: 'Unknown error while updating user alerts'})
+      next(new RequestError(400, 'Could not update alerts', 'Unknown error while updating user alerts'));
     }
   })
 }
