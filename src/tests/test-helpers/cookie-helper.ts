45a07aea210501633b30547ae6d42f5307c193d7
Sebastian Holesz <1081515@ucn.dk>
2021-05-29 12:59:28 +0200

    still trying

--- src/tests/test-helpers/cookie-helper.ts
+++ src/tests/test-helpers/cookie-helper.ts
@@ -1,5 +1,4 @@
 export const extractCookies = (responseHeaders) => {
-  console.log(responseHeaders)
   const cookies = {};
   responseHeaders['set-cookie'].forEach((cookie) => {
     const [name, value] = cookie.split('=');
