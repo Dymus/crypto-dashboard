15ab070fbf3fc1abf073725f144d98a1d222dc15
Sebastian Holesz <1081515@ucn.dk>
2021-05-29 12:43:20 +0200

    trying to solve the empty headers

--- src/tests/test-helpers/cookie-helper.ts
+++ src/tests/test-helpers/cookie-helper.ts
@@ -1,4 +1,5 @@
 export const extractCookies = (responseHeaders) => {
+  console.log(responseHeaders)
   const cookies = {};
   responseHeaders['set-cookie'].forEach((cookie) => {
     const [name, value] = cookie.split('=');
